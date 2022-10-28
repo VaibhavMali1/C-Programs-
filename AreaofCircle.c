adc2cc0dfc53f76bb81e563d6367ee93085b9053
VaibhavMali1 <malivaibhav311@gmail.com>
2022-10-28 22:27:59 +0530

    [ADDED]: Area of Circle Program

---
+++ AreaofCircle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+	 float area,pi=3.14,r=0;
+	 printf("Enter the Radius of Circle:");
+	 scanf("%f",&r);
+	 area=pi*r*r;
+	 printf("\nThe Area of Circle:%f",area);
+}
