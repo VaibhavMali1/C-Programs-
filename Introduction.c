6248adc40379ce17e4ccadc0faf39b3d42145bb8
VaibhavMali1 <malivaibhav311@gmail.com>
2022-11-05 20:02:57 +0530

    [ADDED]:The Introduction Made by Using The Function(Call by Value)

---
+++ Introduction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void introduction()
+{
+    printf("Hi\n");
+    printf("\nMy name is Vaibhav Mali\n");
+    printf("\nI am A Full Stack Developer");
+}
+int main()
+{
+     introduction();
+     return 0;
+}
+     
